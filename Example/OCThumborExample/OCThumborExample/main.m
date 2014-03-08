00efa0506c7e8b3ff842df6878e9581c2b2a0075
Daniel Heckrath <d.heckrath@maple-apps.com>
2014-03-08 09:48:39 +0100

    Initial Commit

---
+++ Example/OCThumborExample/OCThumborExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OCThumborExample
+//
+//  Created by Daniel Heckrath on 08.03.14.
+//  Copyright (c) 2014 Codeserv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
