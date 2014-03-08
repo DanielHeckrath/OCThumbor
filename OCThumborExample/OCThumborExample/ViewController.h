c174192352d067568168f6cc01d3cfb19b75d02c
Daniel Heckrath <d.heckrath@maple-apps.com>
2014-03-08 21:20:15 +0100

    adds exmaple project

---
+++ OCThumborExample/OCThumborExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OCThumborExample
+//
+//  Created by Daniel Heckrath on 08.03.14.
+//  Copyright (c) 2014 Codeserv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
