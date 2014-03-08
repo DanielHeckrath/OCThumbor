78496a42c49f0b60128c61e0174f039d2079aa95
Daniel Heckrath <d.heckrath@maple-apps.com>
2014-03-08 09:50:27 +0100

    Initial Commit

---
+++ Example/Demo/Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by Daniel Heckrath on 08.03.14.
+//  Copyright (c) 2014 Codeserv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
