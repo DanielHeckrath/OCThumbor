9664b4294309fd5154ae786876102d0ee9dcc5e2
Daniel Heckrath <d.heckrath@maple-apps.com>
2014-03-09 01:29:48 +0100

    updates documentation and readme

--- OCThumbor/OCThumbor.h
+++ OCThumbor/OCThumbor.h
@@ -45,23 +45,41 @@
 
 @end
 
+/**
+ Horizontal alignment for crop positioning.
+ */
 typedef NS_ENUM(NSUInteger, ThumborHorizontalAlign) {
     ThumborHorizontalAlignLeft,
     ThumborHorizontalAlignMiddle,
     ThumborHorizontalAlignRight
 };
 
+/**
+ Vertical alignment for crop positioning.
+ */
 typedef NS_ENUM(NSUInteger, ThumborVerticalAlign) {
     ThumborVerticalAlignTop,
     ThumborVerticalAlignMiddle,
     ThumborVerticalAlignBottom
 };
 
+/**
+ Orientation from where to get the pixel color for trim.
+ */
 typedef NS_ENUM(NSUInteger, ThumborTrimPixelColor) {
+    /**
+     top-left
+     */
     ThumborTrimPixelColorTopLeft,
+    /**
+     bottom-right
+     */
     ThumborTrimPixelColorBottomRight
 };
 
+/**
+ Image formats supported by Thumbor.
+ */
 typedef NS_ENUM(NSUInteger, ThumborImageFormat) {
     ThumborImageFormatGif,
     ThumborImageFormatJpeg,
@@ -69,22 +87,41 @@ typedef NS_ENUM(NSUInteger, ThumborImageFormat) {
     ThumborImageFormatWebp
 };
 
+/**
+ Original size for image width or height.
+ */
+UIKIT_EXTERN const NSInteger THUMBOR_ORIGINAL_SIZE;
+
 @interface OCThumborURLBuilder : NSObject
 
+/**
+ Resize picture to desired size.
+ 
+ @param width  Desired width.
+ @param height Desired height.
+ 
+ @throws NSInvalidArgumentException if width or height is less than 0 or both are 0.
+ */
 - (instancetype)resizeWidth:(int)width height:(int)height;
 
 /**
  Flip the image vertically.
+ 
+ @throws NSInternalInconsistencyException if image has not been marked for resize.
  */
 - (instancetype)flipVertically;
 
 /**
  Flip the image horizontally.
+ 
+ @throws NSInternalInconsistencyException if image has not been marked for resize.
  */
 - (instancetype)flipHorizontally;
 
 /**
  Contrain the image size inside the resized box, scaling as needed.
+ 
+ @throws NSInternalInconsistencyException if image has not been marked for resize.
  */
 - (instancetype)fitIn;
 
@@ -95,59 +132,293 @@ typedef NS_ENUM(NSUInteger, ThumborImageFormat) {
  @param left   Left bound.
  @param bottom Bottom bound.
  @param right  Right bound.
+ 
+ @throws NSInvalidArgumentException if top or left are less than zero or bottom or right are less than one or less than top or left, respectively.
  */
 - (instancetype)cropTop:(int)top left:(int)left right:(int)bottom bottom:(int)right;
 
+/**
+ Set the horizontal alignment for the image when cropping.
+ 
+ @param horizontalAlign Horizontal alignment.
+ 
+ @throws NSInternalInconsistencyException if image has not been marked for crop.
+ */
 - (instancetype)horizontalAlign:(ThumborHorizontalAlign)horizontalAlign;
 
+/**
+ Set the vertical alignment for the image when cropping.
+ 
+ @param verticalAlign Vertical alignment.
+ 
+ @throws NSInternalInconsistencyException if image has not been marked for crop.
+ */
 - (instancetype)verticalAlign:(ThumborVerticalAlign)verticalAlign;
 
+/**
+ Set the horizontal and vertical alignment for the image when cropping.
+ 
+ @param verticalAlign   Vertical alignment.
+ @param horizontalAlign Horizontal alignment.
+ 
+ @throws NSInternalInconsistencyException if image has not been marked for crop.
+ */
 - (instancetype)alignVertically:(ThumborVerticalAlign)verticalAlign horizontally:(ThumborHorizontalAlign)horizontalAlign;
 
+/**
+ Use smart cropping for determining the important portion of an image.
+ 
+ @throws NSInternalInconsistencyException if image has not been marked for crop.
+ */
 - (instancetype)smart;
 
+/**
+ Removing surrounding space in image.
+ */
 - (instancetype)trim;
 
+/**
+ Removing surrounding space in image. Get trim color from specified pixel.
+ 
+ @param trimColor orientation from where to get the pixel color.
+ */
 - (instancetype)trim:(ThumborTrimPixelColor)trimColor;
 
+/**
+ Removing surrounding space in image. Get trim color from specified pixel.
+ 
+ @param trimColor orientation from where to get the pixel color.
+ @param tolerance 0 - 442. This is the euclidian distance between the colors of the reference pixel and the surrounding pixels is used. If the distance is within the tolerance they'll get trimmed.
+ */
 - (instancetype)trim:(ThumborTrimPixelColor)trimColor withTolerance:(int)tolerance;
 
+/**
+ Use legacy encryption when constructing a safe URL.
+ */
 - (instancetype)legacy;
 
+/**
+ Add one or more filters to the image.
+ 
+ @throws NSInvalidArgumentException if no arguments supplied or an argument is empty
+ */
 - (instancetype)filter:(NSString *)filterValue, ... NS_REQUIRES_NIL_TERMINATION;
 
+/**
+ Add one or more filters to the image.
+ 
+ @throws NSInvalidArgumentException if filters is empty or an argument is blank
+ */
 - (instancetype)filterFromArray:(NSArray *)filters;
 
+/**
+ Build the URL. This will either call toUrlSafe or toUrlUnsafe depending on whether a key was set.
+ */
 - (NSString *)toUrl;
+
+/**
+ Build an unsafe version of the URL.
+ */
 - (NSString *)toUrlUnsafe;
+
+/**
+ Build a safe version of the URL. Requires a non-nil key.
+ */
 - (NSString *)toUrlSafe;
 
+/**
+ Build the metadata URL. This will either call toMetaSafe or toMetaUnsafe depending on whether a key was set.
+ */
 - (NSString *)toMeta;
+
+/**
+ Build an unsafe version of the metadata URL.
+ */
 - (NSString *)toMetaUnsafe;
+
+/**
+ Build a safe version of the metadata URL. Requires a non-nil key.
+ */
 - (NSString *)toMetaSafe;
 
 #pragma mark - Filter Methods
 
+/**
+ This filter increases or decreases the image brightness.
+ 
+ @param amount -100 to 100 - The amount (in %) to change the image brightness. Positive numbers make the image brighter and negative numbers make the image darker.
+ 
+ @throws NSInvalidArgumentException if amount is outside bounds.
+ */
 + (NSString *)brightness:(int)amount;
+
+/**
+ The filter increases or decreases the image contrast.
+ 
+ @param amount -100 to 100 - The amount (in %) to change the image contrast. Positive numbers increase contrast and negative numbers decrease contrast.
+ 
+ @throws NSInvalidArgumentException if amount is outside bounds.
+ */
 + (NSString *)contrast:(int)amount;
+
+/**
+ This filter adds noise to the image.
+ 
+ @param amount 0 to 100 - The amount (in %) of noise to add to the image.
+ 
+ @throws NSInvalidArgumentException if amount is outside bounds.
+ */
 + (NSString *)noise:(int)amount;
+
+/**
+ This filter changes the overall quality of the JPEG image (does nothing for PNGs or GIFs).
+ 
+ @param amount 0 to 100 - The quality level (in %) that the end image will feature.
+ 
+ @throws NSInvalidArgumentException if amount is outside bounds.
+ */
 + (NSString *)quality:(int)amount;
+
+/**
+ This filter changes the amount of color in each of the three channels.
+ 
+ @param red   The amount of redness in the picture. Can range from -100 to 100 in percentage.
+ @param green The amount of greenness in the picture. Can range from -100 to 100 in percentage.
+ @param blue  The amount of blueness in the picture. Can range from -100 to 100 in percentage.
+ 
+ @throws NSInvalidArgumentException if red, green, or blue are outside of bounds.
+ */
 + (NSString *)red:(int)red green:(int)green blue:(int)blue;
+
+/**
+ This filter adds rounded corners to the image using the white as the background.
+ 
+ @param radius amount of pixels to use as radius.
+ */
 + (NSString *)roundCorner:(int)radius;
+
+/**
+ This filter adds rounded corners to the image using the specified color as the background.
+ 
+ @param radius amount of pixels to use as radius.
+ @param color  fill color for clipped region.
+ */
 + (NSString *)roundCorner:(int)radius color:(int)color;
+
+/**
+ This filter adds rounded corners to the image using the specified color as the background.
+ 
+ @param radiusInner amount of pixels to use as radius.
+ @param radiusOuter specifies the second value for the ellipse used for the radius. Use 0 for no value.
+ @param color       fill color for clipped region.
+ */
 + (NSString *)roundCorner:(int)radiusInner radiusOuter:(int)radiusOuter color:(int)color;
+
+/**
+ This filter adds a watermark to the image.
+ 
+ @param imageUrl
+ 
+ @throws NSInvalidArgumentException if imageUrl is blank
+ */
 + (NSString *)watermark:(NSString *)imageUrl;
+
+/**
+ This filter adds a watermark to the image.
+ 
+ @param imageUrl
+ @param x            Horizontal position that the watermark will be in. Positive numbers indicate position from the left and negative numbers indicate position from the right.
+ @param y            Vertical position that the watermark will be in. Positive numbers indicate position from the top and negative numbers indicate position from the bottom.
+ 
+ @throws NSInvalidArgumentException if imageUrl is blank
+ */
 + (NSString *)watermark:(NSString *)imageUrl x:(int)x y:(int)y;
+
+/**
+ This filter adds a watermark to the image.
+ 
+ @param imageUrl
+ @param x            Horizontal position that the watermark will be in. Positive numbers indicate position from the left and negative numbers indicate position from the right.
+ @param y            Vertical position that the watermark will be in. Positive numbers indicate position from the top and negative numbers indicate position from the bottom.
+ @param transparency Watermark image transparency. Should be a number between 0 (fully opaque) and 100 (fully transparent).
+ 
+ @throws NSInvalidArgumentException if imageUrl is blank or transparency is outside of bounds
+ */
 + (NSString *)watermark:(NSString *)imageUrl x:(int)x y:(int)y transparency:(int)transparency;
+
+/**
+ This filter adds a watermark to the image.
+ 
+ @param builder
+ 
+ @throws NSInvalidArgumentException if builder is nil
+ */
 + (NSString *)watermarkWithBuilder:(OCThumborURLBuilder *)builder;
+
+/**
+ This filter adds a watermark to the image.
+ 
+ @param builder
+ @param x            Horizontal position that the watermark will be in. Positive numbers indicate position from the left and negative numbers indicate position from the right.
+ @param y            Vertical position that the watermark will be in. Positive numbers indicate position from the top and negative numbers indicate position from the bottom.
+ 
+ @throws NSInvalidArgumentException if builder is nil
+ */
 + (NSString *)watermarkWithBuilder:(OCThumborURLBuilder *)builder x:(int)x y:(int)y;
+
+/**
+ This filter adds a watermark to the image.
+ 
+ @param builder      
+ @param x            Horizontal position that the watermark will be in. Positive numbers indicate position from the left and negative numbers indicate position from the right.
+ @param y            Vertical position that the watermark will be in. Positive numbers indicate position from the top and negative numbers indicate position from the bottom.
+ @param transparency Watermark image transparency. Should be a number between 0 (fully opaque) and 100 (fully transparent).
+ 
+ @throws NSInvalidArgumentException if builder is nil
+ */
 + (NSString *)watermarkWithBuilder:(OCThumborURLBuilder *)builder x:(int)x y:(int)y transparency:(int)transparency;
+
+/**
+ This filter enhances apparent sharpness of the image. It's heavily based on Marco Rossini's excellent Wavelet sharpen GIMP plugin. Check http://registry.gimp.org/node/9836 for details about how it work.
+ 
+ @param amount        Sharpen amount. Typical values are between 0.0 and 10.0.
+ @param radius        Sharpen radius. Typical values are between 0.0 and 2.0.
+ @param luminanceOnly Sharpen only luminance channel.
+ */
 + (NSString *)sharpen:(double)amount radius:(double)radius luminanceOnly:(BOOL)luminanceOnly;
+
+/**
+ This filter permit to return an image sized exactly as requested wherever is its ratio by filling with chosen color the missing parts. Usually used with "fit-in" or "adaptive-fit-in"
+ */
 + (NSString *)fill:(int)color;
+
+/**
+ Specify the output format of the image.
+ 
+ @see ThumborImageFormat
+ */
 + (NSString *)format:(ThumborImageFormat)format;
+
+/**
+ This filter uses a 9-patch to overlay the image.
+ 
+ @param imageUrl Watermark image URL. It is very important to understand that the same image loader that Thumbor uses will be used here.
+ */
 + (NSString *)frame:(NSString *)imageUrl;
+
+/**
+ This filter strips the ICC profile from the image.
+ */
 + (NSString *)stripicc;
+
+/**
+ This filter changes the image to grayscale.
+ */
 + (NSString *)grayscale;
+
+/**
+ This filter equalizes the color distribution in the image.
+ */
 + (NSString *)equalize;
 
 @end
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-OCThumbor
+OCThumbor - Objective-C Thumbor client
 =========
+
+Objective-C for the [Thumbor image service][1] which allows you to build URIs
+in an expressive fashion using a fluent API.
+
+This project is largely based on the [Pollexor Library][2] by Square Inc., which is a Thumbor client for JAVA.
+
+License
+=======
+
+    Copyright 2014 Daniel Heckrath
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+[1]: https://github.com/globocom/thumbor
+[2]: http://square.github.io/pollexor/
